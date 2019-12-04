e963a2f86b4f4ca5b3412f62c5a2f0d0bcb2ad43
savybrandt <savy@wonolo.com>
2019-12-03 18:53:42 -0800

    add: teamMember post endpoint

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const { TeamMember } = require('./model');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/team', async (req, res, next) => {
   const team = await TeamMember.findAll();
   return res.json(team);
+});
+
+app.post('/teamMember', async (req, res, next) => {
+  const { body } = req;
+  const teamMember = await TeamMember.create(body);
+  return res.json(teamMember);
 });
 
 module.exports = app;
